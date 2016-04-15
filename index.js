27372c2aa515fcd0b96358cf7726106eaff8c9a9
Matt Brennan <mattyb1000@gmail.com>
2016-04-15 21:57:59 +0100

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+var Trello = require('trello');
+var zooplaScraper = require('zoopla-detail-scraper');
+
+module.exports = function(property, options) {
+	var trello = new Trello(options.applicationKey, options.userToken);
+
+	return zooplaScraper(property)
+	.then(details => trello.addCard(
+			details.address,
+			'#' + details.price + '\n##' + details.blurb + '\n\n' + details.description,
+			options.listId
+		).then(card => Promise.all(
+			details.images
+			.concat([details.floorplan])
+			.filter(image => !!image)
+			.map(image => trello.addAttachmentToCard(card.id, image))
+		).then(() => card))
+	);
+}
