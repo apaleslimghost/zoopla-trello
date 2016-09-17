3aa8e0a06da698b50952f1a9f80ed2ac24ea33d7
Matt Brennan <mattyb1000@gmail.com>
2016-09-17 18:35:33 +0100

    properly handle token non-error errors

--- index.js
+++ index.js
@@ -3,16 +3,8 @@ var zooplaScraper = require('zoopla-detail-scraper');
 
 var getStaticMapUrl = (lat, lon, apiKey) => `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lon}&markers=${lat},${lon}&size=640x640&scale=2&zoom=15&key=${apiKey}`;
 
-var getGoogleMapsUrl = (addr, lat, lon) => `https://www.google.com/maps/place/${encodeURIComponent(addr)}/@${lat},${lon},15z`
-
-module.exports = function(property, options) {
-	var trello = new Trello(options.applicationKey, options.userToken);
-
-	return zooplaScraper(property)
-	.then(details => trello.addCard(
-			details.address,
-			`# ${details.price}
-## [${details.blurb}](${zooplaScraper.toZooplaUrl(property)})
+var cardBody = (details, url, options) => `# ${details.price}
+## [${details.blurb}](${url})
 
 ${details.description}
 
@@ -22,12 +14,30 @@ ${details.description}
 ### Floorplan
 ![Floorplan](${details.floorplan})
 
-`,
+`;
+
+var getGoogleMapsUrl = (addr, lat, lon) => `https://www.google.com/maps/place/${encodeURIComponent(addr)}/@${lat},${lon},15z`
+
+module.exports = function(property, options) {
+	var trello = new Trello(options.applicationKey, options.userToken);
+	var propertyUrl = zooplaScraper.toZooplaUrl(property);
+
+	return zooplaScraper(property)
+	.then(details =>
+		trello.addCard(
+			details.address,
+			cardBody(details, propertyUrl, options),
 			options.listId
-		).then(card => Promise.all(
-			details.images
-			.filter(image => !!image)
-			.map(image => trello.addAttachmentToCard(card.id, image))
-		).then(() => card))
+		).then(card => {
+			if(!card.id) {
+				throw new Error(card);
+			}
+
+			return Promise.all(
+				details.images
+				.filter(image => !!image)
+				.map(image => trello.addAttachmentToCard(card.id, image))
+			).then(() => card);
+		})
 	);
 }
